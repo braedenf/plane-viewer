ed20e68f70e58c54e5ec17b634e4cdb0364632e0
braedenf <braedenleefoster@gmail.com>
2021-11-19 12:58:45 +1300

    Simple plane 3d model viewer

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- Use it like any other HTML element -->
+<model-viewer
+	class="w-screen h-screen"
+	bounds="tight"
+	src="models/bomber.glb"
+	ar
+	ar-modes="webxr scene-viewer quick-look"
+	camera-controls
+	shadow-intensity="2.3"
+	camera-orbit="-114.8deg 82.46deg auto"
+	exposure="0.93"
+>
+	<button
+		slot="ar-button"
+		class="absolute bottom-2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-indigo-500 hover:bg-indigo-600 transition py-2 px-3 rounded-full text-white font-bold"
+	>
+		<div class="flex gap-2">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"
+				/>
+			</svg>
+			<label for="ar-button">Activate AR</label>
+		</div>
+	</button>
+</model-viewer>
